c716993936916f64d29748311110fd14ccd4a1b6
jaitches <36822351+jaitches@users.noreply.github.com>
2021-08-05 15:50:21 +0100

    add providers

--- app/routes.js
+++ app/routes.js
@@ -21,66 +21,153 @@ router.post('/index-figures', function (req, res) {
 
 router.get('/display-pensions', function (req, res) {
 // connect to MongoDB to add the doc (record) to the collection (table)
-    async function listPensions() {
-    // create an instance of the client
-        const client = new MongoClient(uri)
-            try {
-                // Connect to the MongoDB cluster
-                await client.connect()
+    async function findPensions() {
+//       const uri = "mongodb+srv://all_dbs_user:U5oZLxA850eM8TFr@cluster0.de9k1.mongodb.net/pensions?retryWrites=true&w=majority";
+        
+        const client = new MongoClient(uri);
 
-                await getPensions(client)
-            }
-            catch (error) {
-                console.error(error)
-            }
-            finally {
-                // Close the connection to the MongoDB cluster
- //               await client.close()
-            }
+        try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+            req.app.locals.pensionDetails = await getPensions(client);
+        } finally {
+            // Close the connection to the MongoDB cluster
+            await client.close();    
+            res.render('display-pensions')
+        }
     }
+
+    findPensions().catch(console.error);
+
     async function getPensions(client) {
-    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-//        try {
         const results = await client.db("pensions").collection("pensionDetails")
+        // find all documents
         .find({})
-        .toArray(function(err, pensionArray) {
-            if (err) throw err;
-            console.log(pensionArray)
-            req.app.locals.pensionDetails = pensionArray
-        })
-/* 
-        if ((await cursor.count()) === 0) {
-            console.log("No documents found")
-        }
-       }
-        catch (error){
-            console.log('error finding ' + error)
-        }
+        // save them to an array
+        .sort({pensionOwner: 1, accruedType: 1})        
+        .toArray()
 
-*/
-//            .sort({ last_review: -1 })
-//            .limit(maximumNumberOfResults);
 
+//        console.log('results ' + JSON.stringify(results))
+        return results
     }
-    listPensions().catch(console.error);
-
-    res.render('display-pensions')
 })
 
-router.get('/manage-pensions', function (req, res) {
-	let pensionArray = []
-	console.log('pensionList.pensions.length ' + pensionList.pensions.length)
-	for (let i=0; i < pensionList.pensions.length; i++) {
-        pension = pensionList.pensions[i]
-        pensionArray.push(pension) 
-	}
-	req.app.locals.pensionDetails = pensionArray
+router.post('/add-provider-details', function (req, res) {
+//get the pension provider list
 
-  res.render('manage-pensions')
+// format date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var hh = String(today.getHours()).padStart(2, '0')
+    var min = String(today.getMinutes()).padStart(2, '0')
+
+    todayDateTime = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + min
+    let addAdministratorName = req.session.data['administrator-name']
+    let addAdministratorURL = "https://" + addAdministratorName.toLowerCase().replace(/" "/g,"") + ".co.uk"
+    let addAdministratorEmail = "info@" + addAdministratorName.toLowerCase().replace(/" "/g,"") + ".co.uk"
+    let addAdministratorPhoneNumber = "01" + Math.floor(Math.random() * 1000).toString().substring(1,3) + " 020500"
+    let addAdministratorAnnualReportURL = "https://" + addAdministratorName.toLowerCase().replace(/" "/g,"") + "/annual-report.co.uk"
+    let addAdministratorCostChargesURL = "https://" + addAdministratorName.toLowerCase().replace(/" "/g,"") + "/costs-and-charges.co.uk"
+    let addAdministratorImplementationURL = "https://" + addAdministratorName.toLowerCase().replace(/" "/g,"") + "/implementation-statement.co.uk"
+    let addAdministratorSIPURL = "https://" + addAdministratorName.toLowerCase().replace(/" "/g,"") + "/statement-of-investment.co.uk"
+
+// connect to MongoDB to add the doc (record) to the collection (table)
+    async function addProvider() {
+    // create an instance of the client
+        const client = new MongoClient(uri);
+
+        try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+
+            // Make the appropriate DB calls
+            await createPension(client, {
+
+                administratorName : addAdministratorName,
+                administratorContactPreference : "email",
+                administratorURL : addAdministratorURL,
+                administratorEmail : addAdministratorEmail,
+                administratorPhoneNumber : "Enquiries",
+                administratorPhoneNumberType: "",
+                admisistratorPostalName : addAdministratorName,
+                administratorAddressLine1 : "Floor 21",        
+                administratorAddressLine2 : "Tall Tower",
+                administratorAddressLine3 : "High Street",
+                administratorAddressLine4 : "Trumpton", 
+                administratorAddressLine5 : "",
+                administratorPostcode : "TR7 5DS",
+                administratorAnnualReportURL : addAdministratorAnnualReportURL,
+                administratorCostChargesURL : addAdministratorCostChargesURL,
+                administratorImplementationURL : addAdministratorImplementationURL,
+                administratorSIPURL : addAdministratorSIPURL,
+                dateAdded: todayDateTime
+
+            });
+        } finally {
+            // Close the connection to the MongoDB cluster
+            await client.close()
+            res.redirect('add-providers')
+        }
+    }
+
+    addProvider().catch(console.error);
+
+    // Add functions that make DB calls here
+    async function createPension(client, newListing){
+        const result = await client.db("pensions").collection("pensionProvider").insertOne(newListing);
+        console.log(`New listing created with the following id: ${result.insertedId}`);
+    } 
+
+})
+router.get('/add-pensions', function (req, res) {
+    req.app.locals.pensionProviders = []
+    async function findPensionProviders() {        
+        const client = new MongoClient(uri);
+
+        try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+            req.app.locals.pensionProviders = await getProviders(client)
+            console.log('pensionProviders ' + req.app.locals.pensionProviders)
+        } finally {
+            // Close the connection to the MongoDB cluster
+            await client.close();    
+            res.render('add-pensions')
+        }
+    }
+
+    findPensionProviders().catch(console.error);
+
+    async function getProviders(client) {
+        const results = await client.db("pensions").collection("pensionProvider")
+        // find all documents
+        .find({})
+        // save them to an array
+        .sort({administratorName: 1})        
+        .toArray()
+
+        console.log('results ' + JSON.stringify(results))
+        return results
+    }
+
 })
 
-router.post('/add-pension', function (req, res) {
-// get the inputted pension data 
+router.post('/add-pension-details', function (req, res) {
+//get the pension provider list
+
+// format date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var hh = String(today.getHours()).padStart(2, '0')
+    var min = String(today.getMinutes()).padStart(2, '0')
+
+    todayDateTime = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + min
+    // get the inputted pension data 
     let addPensionOwner = req.session.data['pension-owner']
     let addPensionName = req.session.data['pension-name']
     let addAdministratorName = req.session.data['pension-provider']
@@ -88,7 +175,6 @@ router.post('/add-pension', function (req, res) {
     let addERIPayableDate = req.session.data['date-payable']
     let addERIAnnualAmount = req.session.data['annual-eri']
     let addERIPot = req.session.data['pension-value']
-// create a doc to add to the DB
 
 // connect to MongoDB to add the doc (record) to the collection (table)
     async function addPension() {
@@ -101,17 +187,21 @@ router.post('/add-pension', function (req, res) {
 
             // Make the appropriate DB calls
             await createPension(client, {
+
+                pensionOwner : addPensionOwner,
                 pensionName : addPensionName,
                 administratorName : addAdministratorName,
                 ERICalculationDate : addERICalculationDate,
                 ERIPayableDate : addERICalculationDate,
                 ERIAnnualAmount : Number(addERIAnnualAmount),
-                ERIPot: Number(addERIPot)
+                ERIPot: Number(addERIPot),
+                dateAdded: todayDateTime
 
             });
         } finally {
             // Close the connection to the MongoDB cluster
-            await client.close();
+            await client.close()
+            res.redirect('add-pensions')
         }
     }
 
@@ -121,13 +211,10 @@ router.post('/add-pension', function (req, res) {
     async function createPension(client, newListing){
         const result = await client.db("pensions").collection("pensionDetails").insertOne(newListing);
         console.log(`New listing created with the following id: ${result.insertedId}`);
-    }
-
-
-    res.render('manage-pensions')
-
+    } 
 
 })
+
 router.post('/edit-pension', function (req, res) {
 	let editPensionArray =[]
 
--- readPensions.js
+++ readPensions.js
@@ -1,26 +1,14 @@
 const { MongoClient } = require('mongodb');
 
+
 async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://all_dbs_user:U5oZLxA850eM8TFr@cluster0.de9k1.mongodb.net/pensions?retryWrites=true&w=majority";
+   const uri = "mongodb+srv://all_dbs_user:U5oZLxA850eM8TFr@cluster0.de9k1.mongodb.net/pensions?retryWrites=true&w=majority";
     
-    /**
-     * The Mongo Client you will use to interact with your database
-     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
-     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
-     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
-     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
-     */
     const client = new MongoClient(uri);
 
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
-        // Make the appropriate DB calls
         await getPensions(client);
     } finally {
         // Close the connection to the MongoDB cluster
@@ -30,25 +18,10 @@ async function main() {
 
 main().catch(console.error);
 
-// Add functions that make DB calls here
 async function getPensions(client) {
-// See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-//        try {
-    const results = await client.db("pensions").collection("lpensionDetails")
-    .find()
+    const results = await client.db("pensions").collection("pensionDetails")
+    .find({})
     .toArray()
-/* 
-    if ((await cursor.count()) === 0) {
-        console.log("No documents found")
-    }
-   }
-    catch (error){
-        console.log('error finding ' + error)
-    }
-
-*/
-//            .sort({ last_review: -1 })
-//            .limit(maximumNumberOfResults);
 
     // Store the results in an array
     console.log('results ' + JSON.stringify(results))
